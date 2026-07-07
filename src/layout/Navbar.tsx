9c30bdc88ffa9563c1b8a6809ed9c19bbc6d5fc3
zied3332 <141698161+zied3332@users.noreply.github.com>
2026-07-07 18:28:43 +0100

    feat: improve responsive navbar and complete homepage previews

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,43 +1,49 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Ticket } from "lucide-react";
-import {
-  FaInstagram,
-  FaFacebookF,
-  FaYoutube,
-  FaTiktok,
-} from "react-icons/fa6";
+import { Menu, X, Ticket } from "lucide-react";
+import { FaInstagram, FaFacebookF, FaYoutube, FaTiktok } from "react-icons/fa6";
 import "./Navbar.css";
 import logo from "./logo.png";
+
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <header className="navbar">
       <nav className="navbar__inner">
-      <Link to="/" className="navbar__brand">
-  <img
-    src={logo}
-    alt="Waterfall Festival"
-    className="navbar__logo"
-  />
-</Link>
+        <Link to="/" className="navbar__brand" onClick={closeMenu}>
+          <img src={logo} alt="Waterfall Festival" className="navbar__logo" />
+        </Link>
 
-        <div className="navbar__links">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/events">Events</NavLink>
-          <NavLink to="/tickets">Tickets</NavLink>
-          <NavLink to="/experience">Experience</NavLink>
-          <NavLink to="/gallery">Gallery</NavLink>
-          <NavLink to="/info">Info</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+        <button
+          className="navbar__menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle menu"
+        >
+          {menuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        <div className={`navbar__links ${menuOpen ? "navbar__links--open" : ""}`}>
+          <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+          <NavLink to="/events" onClick={closeMenu}>Events</NavLink>
+          <NavLink to="/tickets" onClick={closeMenu}>Tickets</NavLink>
+          <NavLink to="/experience" onClick={closeMenu}>Experience</NavLink>
+          <NavLink to="/gallery" onClick={closeMenu}>Gallery</NavLink>
+          <NavLink to="/venue" onClick={closeMenu}>Venue</NavLink>
+          <NavLink to="/faq" onClick={closeMenu}>FAQ</NavLink>
+          <NavLink to="/contact" onClick={closeMenu}>Contact</NavLink>
         </div>
 
         <div className="navbar__right">
-      <div className="navbar__socials">
-  <FaInstagram size={18} />
-  <FaFacebookF size={16} />
-  <FaYoutube size={19} />
-  <FaTiktok size={17} />
-</div>
+          <div className="navbar__socials">
+            <FaInstagram size={18} />
+            <FaFacebookF size={16} />
+            <FaYoutube size={19} />
+            <FaTiktok size={17} />
+          </div>
+
           <select className="navbar__lang">
             <option>EN</option>
             <option>FR</option>
